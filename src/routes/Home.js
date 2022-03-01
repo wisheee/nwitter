912d7f7cdbbea54db1b6154cd88e8153f42ac73d
wisheee <wishee.u@gmail.com>
2022-03-01 22:56:11 +0900

    feat: firestore addDoc 추가(setNweet)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "fbase";
+import { addDoc, collection } from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, 'nweets'), {
+        nweet,
+        createdAt: Date.now()
+      });
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+    setNweet('');
+  };
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input 
+          value={nweet}
+          onChange={onChange}
+          type="text" 
+          placeholder="What's on your mind?" 
+          maxLength={120} 
+        />
+        <button>Nweet</button>
+      </form>
+    </div>
+  );
+};
 export default Home;
