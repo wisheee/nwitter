3eef9a17e32b05254142210493b554b4493c8592
wisheee <wishee.u@gmail.com>
2022-03-01 22:12:32 +0900

    feat: sign out 기능 구현

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
-          <Route path="/" element={<Home />} />
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </>
         ) : (
           <Route path="/" element={<Auth />} />
         )}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { authService } from "fbase";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate('/');
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 export default Profile;
