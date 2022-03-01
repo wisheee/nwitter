354fe6b69f1cf17da02a7f67d4db5c64408bb246
wisheee <wishee.u@gmail.com>
2022-03-01 11:47:10 +0900

    feat: Absolute Imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
-import AppRouter from "./Router";
+import { useState } from "react";
+import AppRouter from "components/Router";
+import firebase from "fbase";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <AppRouter />
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
   );
 }
 
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
       <Routes>
