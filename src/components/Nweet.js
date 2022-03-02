b004b1f119a59daa5f439c40618786325a2f537a
wisheee <wishee.u@gmail.com>
2022-03-02 16:59:46 +0900

    feat: firestore updateDoc 추가

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { dbService } from "fbase";
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 
 const Nweet = ({ nweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
   const onDeleteClick = async () => {
     const ok = window.confirm('Are you sure you want to delete this nweet?');
     if (ok) {
       await deleteDoc(doc(dbService, 'nweets', nweetObj.id));
     }
   };
+  const toggleEditing = () => setEditing(prev => !prev);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    setNewNweet(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await updateDoc(doc(dbService, 'nweets', nweetObj.id), {
+      text: newNweet
+    });
+    setEditing(false);
+  };
 
   return (
     <div>
-      <h4>{nweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button onClick={onDeleteClick}>Delete Nweet</button>
-          <button>Edit Nweet</button>
+          <form onSubmit={onSubmit}>
+            <input 
+              type="text"
+              placeholder="Edit your nweet"
+              value={newNweet}
+              required
+              onChange={onChange}
+            />
+            <button>Update Nweet</button>
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
         </>
       )}
     </div>
