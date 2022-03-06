3a59ca8d3008567785616e778c18292ad36482b4
wisheee <wishee.u@gmail.com>
2022-03-06 22:41:31 +0900

    feat: 전역 스타일 추가

---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background-color: rgb(15, 20, 25);
+    color: #fff;
+  }
+`;
+
+export default GlobalStyles;
